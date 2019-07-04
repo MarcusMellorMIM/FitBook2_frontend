7ca250a382d320911414b52b821fbadf39492915
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-07-04 17:15:41 +0100

    basic navbar with routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./NavBar";
+import Signup from "./Signup";
+import Login from "./Login";
+import Weight from "./Weight";
+import Food from "./Food";
+import Account from "./Account";
+import Exercise from "./Exercise";
+import Dashboard from "./Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      weights: [],
+      meals: [],
+      exercises: [],
+      isLoggedIn: false
+    };
+  }
+
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <Route path="/Login" component={Login} />
+        <Route path="/Signup" component={Signup} />
+        <Route path="/Weight" component={Weight} />
+        <Route path="/Food" component={Food} />
+        <Route path="/Dashboard" component={Dashboard} />
+        <Route path="/Account" component={Account} />
+        <Route path="/Exercise" component={Exercise} />
+      </Router>
+    );
+  }
 }
 
 export default App;
