2d8f468c4e7e9b3b1c29009b946dbbd2a34157b4
MarcusMellorMIM <marcus@mellor.com>
2019-07-07 16:45:15 +0100

    Added weight CRUD

--- src/App.js
+++ src/App.js
@@ -8,79 +8,192 @@ import Account from "./Account";
 import Exercise from "./Exercise";
 import Dashboard from "./Dashboard";
 
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
+const WEIGHTS_URL = "http://localhost:3000/weights";
+const USERS_URL = "http://localhost:3000/users";
 
+const EMPTYWEIGHT = {
+  id: "",
+  weight_kg: "",
+  weight_date: null,
+  weight_d: "",
+  weight_t: ""
+};
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: {"id": 1,
-      "user_name": "marcus",
-      "password_digest": null,
-      "email": null,
-      "name": "Marcus",
-      "dob": "1967-06-13T00:00:00.000Z",
-      "height_cm": 176,
-      "gender": "M",
-      "created_at": "2019-07-04T14:39:30.820Z",
-      "updated_at": "2019-07-04T14:39:30.820Z"},
-      weights: [{
-        "id": 1,
-        "user_id": 1,
-        "weight_kg": 103.0,
-        "weight_date": "2019-07-01T00:00:00.000Z",
-        "created_at": "2019-07-05T10:25:44.047Z",
-        "updated_at": "2019-07-05T10:25:44.047Z"
-      },
-              {"id": 2,
-              "user_id": 1,
-              "weight_kg": 102.0,
-              "weight_date": "2019-07-02T00:00:00.000Z",
-              "created_at": "2019-07-05T10:26:26.026Z",
-              "updated_at": "2019-07-05T10:26:26.026Z"},
-              {"id": 3,
-              "user_id": 1,
-              "weight_kg": 101.5,
-              "weight_date": "2019-07-03T00:00:00.000Z",
-              "created_at": "2019-07-05T10:27:50.736Z",
-              "updated_at": "2019-07-05T10:27:50.736Z"},
-            {"id": 4,
-            "user_id": 1,
-            "weight_kg": 102.5,
-            "weight_date": "2019-07-04T00:00:00.000Z",
-            "created_at": "2019-07-05T10:27:59.842Z",
-            "updated_at": "2019-07-05T10:27:59.842Z"},
-          {"id": 5,
-          "user_id": 1,
-          "weight_kg": 103.25,
-          "weight_date": "2019-07-05T00:00:00.000Z",
-          "created_at": "2019-07-05T10:28:21.051Z",
-          "updated_at": "2019-07-05T10:28:21.051Z"}],
+      user: {},
+      weight: EMPTYWEIGHT,
+      weights: [],
       meals: [],
       exercises: [],
       isLoggedIn: true
     };
   }
 
- createWeight = event => {
-    event.preventDefault();
-    console.log(event)
+  componentDidMount() {
+    // THIS SHOULD BE REPLACED BY THE FUNCTION THAT LOGS ON A USER
+    fetch(`${USERS_URL}/marcus`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ user: data });
+      });
+
+    // SHOULD WE FETCH ALL IN USERS ??? OR SEPERATE DATA ???
+    fetch(`${WEIGHTS_URL}/user/marcus`)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ weights: data });
+      });
   }
-  
+
+  // WEIGHT HANDLERS START
+  // All of the weight handlers ... should these be in a seperate file etc ?? whats best practice ?
+  changeWeight = event => {
+    // The handler that changes the weight state, for either new or updates of a weight
+    let weight = {};
+    Object.assign(weight, this.state.weight);
+    weight[event.target.name] = event.target.value;
+    this.setState({ weight: weight });
+  };
+
+  deleteWeight = () => {
+    // The handler that changes the weight state, for either new or updates of a weight
+       console.log("Deleting weight");
+    let weights = []
+
+    fetch(`${WEIGHTS_URL}/${this.state.weight.id}`, {method:"DELETE"} )
+    .then(() => {
+      console.log(`delete ${this.state.weight.id}`)
+      weights = this.state.weights.filter(w=>w.id!==this.state.weight.id)
+      this.setState({ weights: weights })
+      this.setState({ weight: EMPTYWEIGHT })
+    });
+  };
+
+
+  submitWeight = event => {
+    // Used to create a new weight, or update an existing one
+    event.preventDefault();
+    // Should sort out the date stuff in the onChange hanlder .....
+    //    let date = new Date();
+    // date.setDate(07); AAAARRRRGGGHHHHH
+    //date.setMonth(07);
+    //date.setFullYear(2019);
+    // date.setHours(09);
+    // date.setMinutes(30);
+
+    let weights = [...this.state.weights];
+
+    // 1st sort out the date, will work on including time later
+    // this.state.weight_d ? date = Date.now() : date = Date.parse(this.state.weight_d)
+    // this.state.weight_t==="" ? :
+    let weight = {
+      id:this.state.weight.id,
+      user_id: this.state.user.id,
+      weight_kg: this.state.weight.weight_kg,
+      weight_date: this.state.weight_date
+    };
+
+    if (this.state.weight.id === "") {
+      // Inserting a weight
+      console.log("New weight");
+
+      if (!weight.weight_date) { weight.weight_date = new Date(); }
+
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify(weight)
+      };
+
+      fetch(WEIGHTS_URL, configObj)
+        .then(data => data.json())
+        .then(data => {
+          weights.push(data);
+          console.log(weights);
+          return weights;
+        })
+        .then(weights => {
+          this.setState({ weights: weights });
+        });
+    } else {
+      //Updating a weight in the database TO BE TESTED
+      console.log("Update weight");
+      let configObj = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify(weight)
+      };
+
+      fetch(`${WEIGHTS_URL}/${weight.id}`, configObj)
+        .then(data => data.json())
+        .then(weight => {
+          let weightIndex = weights.findIndex(w => w.id === weight.id);
+          Object.assign(weights[weightIndex], weight);
+          this.setState({ weights: weights });
+        });
+
+    }
+// Whether inserting or updating, we need to reset the weight state to 
+// get the weight entry/udpate/delete form into initial state
+    this.setState({ weight: EMPTYWEIGHT });
+  };
+
+  selectWeight = (datapoint, event) => {
+    let weight = this.state.weights.find(w=>w.id===datapoint.id)
+    this.setState({weight:weight})
+  }
+
+// END OF WEIGHT STATE HANDLERS ETC
+
+// START OF USER STATE HANDLERS
   createUser = event => {
     event.preventDefault();
-    console.log(event)
-  }
-  
+    console.log(event);
+  };
+
+  // END OF USER STATE HANDLERS
+
+// Render the pages, with routes called from the selection from Navbar
   render() {
     return (
       <Router>
         <NavBar />
         <Route path="/Login" component={Login} />
-        <Route path="/Signup" render={() => <Signup user={this.state.user} isLoggedIn={this.state.isLoggedIn} createUser={this.createUser}/>} />
-        <Route path="/Weight" render={() => <Weights user={this.state.user} weights={this.state.weights} createWeight={this.createWeight}/>} />
+        <Route
+          path="/Signup"
+          render={() => (
+            <Signup
+              user={this.state.user}
+              isLoggedIn={this.state.isLoggedIn}
+              createUser={this.createUser}
+            />
+          )}
+        />
+        <Route
+          path="/Weight"
+          render={() => (
+            <Weights
+              user={this.state.user}
+              weights={this.state.weights}
+              weight={this.state.weight}
+              submitWeight={this.submitWeight}
+              changeWeight={this.changeWeight}
+              deleteWeight={this.deleteWeight}
+              selectWeight={this.selectWeight}
+            />
+          )}
+          deleteWeight={this.deleteWeight}
+        />
         <Route path="/Food" component={Food} />
         <Route path="/Dashboard" component={Dashboard} />
         <Route path="/Account" component={Account} />
--- src/Weights.js
+++ src/Weights.js
@@ -1,56 +1,101 @@
 import React, { Component } from "react";
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalGridLines, LineSeries} from 'react-vis';
-
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  VerticalGridLines,
+  MarkSeries
+} from "react-vis";
 
 class Weight extends Component {
   constructor() {
-    super()
+    super();
   }
 
   genGraphData = () => {
+    // Render the weight object into a form the graph object understands
     let data = this.props.weights.map(weight => {
-      return { x: Date.parse(weight.weight_date),
-              y: weight.weight_kg }
-      })
-      console.log(data);
-      return data;
-  }
+      return { x: Date.parse(weight.weight_date), y: weight.weight_kg, id:weight.id };
+    });
+
+    return data;
+  };
 
   render() {
-    return (
-    <div>
-      <div>
-        <form onSubmit={this.props.onSubmitForm}>
-          <label name="weight_kg" >Please enter your latest weight  </label>
-          <input type="number" name="weight_kg" />
-          <label name="weight_kg" > in kilograms.</label>
-          <br/>
-          <label name="weight_date">Enter the date  </label>
-          <input type="date" name="weight_date"/>
-          <label name="weight_date"> or leave blank if recorded today.</label>
-          <br/>
-          <label name="weight_time">Enter the time  </label>
-          <input type="time" name="weight_time"/>
-          <label name="weight_date"> or leave blank if taken now.</label>
-          <br/>
-          <label name="submitBtn">Hit submit, to save this entry</label>
-          <button type="submit" name="submitBtn">Submit</button>
-        </form>
-        <hr/>
-      </div>
+    const { id, weight_kg, weight_d, weight_t } = this.props.weight;
 
+    return (
       <div>
-        <div className="graph" >
-            <XYPlot xType="time" height={300} width={500}>
+        <div>
+          <form onSubmit={this.props.submitWeight}>
+            <input type="hidden" id="weight_id" name="weight_id" value={id} />
+            <label htmlFor="weight_kg">Please enter your latest weight </label>
+            <input
+              type="number"
+              id="weight_kg"
+              name="weight_kg"
+              value={weight_kg}
+              onChange={this.props.changeWeight}
+            />
+            <label name="weight_kg"> in kilograms.</label>
+            <br />
+            <label htmlFor="weight_d">Enter the date </label>
+            <input
+              type="date"
+              id="weight_d"
+              name="weight_d"
+              value={weight_d}
+              onChange={this.props.changeWeight}
+            />
+            <label name="weight_date"> or leave blank if recorded today.</label>
+            <br />
+            <label htmlFor="weight_t">Enter the time </label>
+            <input
+              type="time"
+              id="weight_t"
+              name="weight_t"
+              value={weight_t}
+              onChange={this.props.changeWeight}
+            />
+            <label name="weight_date"> or leave blank if taken now.</label>
+            <br />
+            <label htmlFor="submitBtn">Hit submit, to save this entry</label>
+            <button type="submit" id="submitBtn" name="submitBtn">
+              Submit
+            </button>
+
+          </form>
+
+            <label htmlFor="deleteBtn">Hit delete, to remove this entry</label>
+            <button type="submit"id="deleteBtn" name="deleteBtn" onClick={this.props.deleteWeight}>
+              Delete
+            </button>
+
+          <hr />
+        </div>
+
+        <div>
+          <div>
+            <XYPlot
+              xType="time"
+              height={400}
+              width={500}
+              margin={{ left: 120, right: 20, top: 20, bottom: 40 }}
+            >
               <HorizontalGridLines />
               <VerticalGridLines />
               <XAxis title="Time" />
-              <YAxis title="Weight (kg)" />
-              <LineSeries data={this.genGraphData()} />
+              <YAxis title="Weight (kg)" width={70} />
+              <MarkSeries
+                color="red"
+                data={this.genGraphData()}
+                onValueClick= {(datapoint,event) => this.props.selectWeight(datapoint,event)}
+              />
             </XYPlot>
+          </div>
         </div>
       </div>
-</div>
     );
   }
 }
